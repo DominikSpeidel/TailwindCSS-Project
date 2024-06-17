b335d2b122dfaa0ae3057088b0cb4a0e4c6bc7eb
Dominik Speidel <dominik-speidel@posteo.de>
2024-06-17 13:37:53 +0200

    Links Footer implemented

--- pages/components/footer.js
+++ pages/components/footer.js
@@ -1,15 +1,17 @@
+import Link from "next/link";
+
 function Footer() {
   return (
     <footer className="md:col-span-3 md:col-start-2 mt-14 mb-20">
       <ul className="flex text-center justify-evenly ">
         <li className="hover:text-gray-900">
-          <a href="/">Impressum</a>
+          <Link href="/impressum">Impressum</Link>
         </li>
         <li className="hover:text-gray-900">
-          <a href="/">Datenschutz</a>
+          <Link href="/datenschutz">Datenschutz</Link>
         </li>
         <li className="hover:text-gray-900">
-          <a href="/">Karriere</a>
+          <Link href="/karriere">Karriere</Link>
         </li>
         <li className="hover:text-gray-900">
           <a href="/">© 2024</a>
