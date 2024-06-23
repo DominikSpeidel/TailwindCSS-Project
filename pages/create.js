aab917e9827c2749f2da2c0c577d2ea93b3c6bbd
Dominik Speidel <dominik-speidel@posteo.de>
2024-06-23 13:02:19 +0200

    Textarea in CreateForm implemented

--- pages/create.js
+++ pages/create.js
@@ -157,7 +157,21 @@ export default function Create() {
                   )}
                 </section>
               </article>
+              <div className="mb-5">
+                <label
+                  htmlFor="Beschreibung"
+                  className="block mb-2 text-lg font-medium text-gray-900 dark:text-white"
+                >
+                  Beschreibung
+                </label>
+                <textarea
+                  id="Beschreibung"
+                  rows="8"
+                  className="min-w-[14rem] lg:min-w-72 shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-lg rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+                ></textarea>
+              </div>
             </div>
+
             <div className=" xl:px-20 mx-auto mb-10">
               <div className="flex flex-col xl:items-center">
                 <h3 className="mb-2 text-lg font-medium text-gray-900 dark:text-white">
