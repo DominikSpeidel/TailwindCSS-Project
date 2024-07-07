f6320f018b29fd57d4b8a32b531379cfe504478b
Dominik Speidel <dominik-speidel@posteo.de>
2024-07-07 19:07:38 +0200

    changed Imagetag

--- pages/components/card.js
+++ pages/components/card.js
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Clock_Icon from "./Icons/Clock_Icon";
 import Link from "next/link";
 
@@ -5,10 +6,13 @@ export default function Card({ id, name, duration, imageUrl, user }) {
   return (
     <Link href={`/${id}`}>
       <div className="card hover:shadow-xl ">
-        <img
+        <Image
           className="w-full h-64 object-cover transform hover:scale-105 duration-500"
           src={imageUrl}
+          width={800}
+          height={600}
           alt="Hills"
+          priority
         />
         <div className="flex flex-col justify-evenly text-center pt-2 h-20">
           <p>{name}</p>
