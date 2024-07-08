4fc4a167d4f2abe367f12eab6848235d82e1aa9a
Dominik Speidel <dominik-speidel@posteo.de>
2024-07-08 21:49:09 +0200

    Deployment bug

--- pages/components/NavLink.js
+++ pages/components/NavLink.js
@@ -5,9 +5,7 @@ export default function NavLink({ href, text, Icon, isActive }) {
     <li className="py-1 border-r-2 border-white hover:border-primary">
       <Link
         href={href}
-        className={`px-4 flex justify-end ${
-          isActive ? "text-gray-900" : undefined
-        }`}
+        className={`px-4 flex justify-end ${isActive && "text-gray-900"}`}
       >
         <span className="mr-2">{text}</span>
         <Icon />
